9fb8a0051aa8475c577d46561437cf49e6287e79
Vipin Nair <swvist@gmail.com>
2013-01-22 03:28:16 +0530

    Adding last user cred

--- settings.py
+++ settings.py
@@ -11,15 +11,15 @@ GA_CODE=''
 #: Database backend
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 #: Secret key
-SECRET_KEY = 'make this something random'
+SECRET_KEY = 'SvSGkI2jTTiThEnaHovGpAH_51m6tDTfiG4R2FaOD8qA2'
 #: Timezone
 TIMEZONE = 'Asia/Calcutta'
 #: LastUser server
 LASTUSER_SERVER = 'https://auth.hasgeek.com'
 #: LastUser client id
-LASTUSER_CLIENT_ID = ''
+LASTUSER_CLIENT_ID = 'QzjI90_IRmGy4EnLHncTpA'
 #: LastUser client secret
-LASTUSER_CLIENT_SECRET = ''
+LASTUSER_CLIENT_SECRET = 'SvSGkI2jTTiThEnaHovGpAH_51m6tDTfiG4R2FaOD8qA'
 #: Mail settings
 #: MAIL_FAIL_SILENTLY : default True
 #: MAIL_SERVER : default 'localhost'
